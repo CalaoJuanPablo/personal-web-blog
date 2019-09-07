2badf6fdd265a7905bebb0dafd3498dcc12f07a1
Juan Pablo Calao Perez <jcalaop@gmail.com>
2019-09-07 10:34:18 -0500

    twitter card test

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -8,6 +8,11 @@ const Layout = ({ children, title, description }) => (
       <html lang="es" />
       <title>{title}</title>
       <meta name="description" content={description} />
+      <meta name="twitter:card" content="summary" />
+      {/* <meta property="og:url" content="http://bits.blogs.nytimes.com/2011/12/08/a-twitter-for-my-sister/" /> */}
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      {/* <meta property="og:image" content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg" /> */}
     </Helmet>
     {children}
   </div>
