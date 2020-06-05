f359da28fd0dc9be80861ece15700a6f2eeabae1
Juan Pablo Calao Perez <jcalaop@gmail.com>
2020-06-05 13:07:45 -0500

    set date for post in spanish

--- src/components/Post/Meta/Meta.js
+++ src/components/Post/Meta/Meta.js
@@ -2,10 +2,17 @@ import React from 'react';
 import moment from 'moment';
 import styles from './Meta.module.scss';
 
-const Meta = ({ date }) => (
-  <div className={styles['meta']}>
-    <p className={styles['meta__date']}>Publicado {moment(date).format('D MMM YYYY')}</p>
-  </div>
-);
+const Meta = ({ date }) => {
+  const metaOptionsForDate = {
+    month: 'long',
+    day: 'numeric',
+    year: "numeric"
+  }
+  return (
+    <div className={styles['meta']}>
+      <p className={styles['meta__date']}>Publicado {new Date(date).toLocaleDateString("es", metaOptionsForDate)}</p>
+    </div>
+  );
+}
 
 export default Meta;
