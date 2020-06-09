74668049e9e5bbdb9f75001e2ba46fc9e6d6ba95
Juan Pablo Calao Perez <jcalaop@gmail.com>
2020-06-08 22:20:11 -0500

    add header menu mobile open/close logic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,28 @@
 // Dependencies
-import React from 'react';
-import { Link } from 'gatsby';
+import React from 'react'
+import { Link } from 'gatsby'
 // Styles
-import styles from './Header.module.scss';
+import styles from './Header.module.scss'
 
 export function Header() {
+	let [menuIsOpen, setMenuIsOpen] = React.useState(false)
+
+	function handleIsMenuOpen(event) {
+		event.preventDefault()
+
+		setMenuIsOpen(!menuIsOpen)
+	}
+
 	return (
-		<header className={styles['Header']}>
+		<header className={styles['Header']} data-state={menuIsOpen}>
 			<figure className={styles['Header__logo']}>
 				<img src="" alt="Logo" />
 			</figure>
 			<nav className={styles['Header__nav']}>
-				<div className={styles['Header__nav__menu-icon']}>
+				<div
+					className={styles['Header__icon']}
+					onClick={handleIsMenuOpen}
+				>
 					<i></i>
 					<i></i>
 					<i></i>
@@ -37,5 +48,5 @@ export function Header() {
 				</ul>
 			</nav>
 		</header>
-	);
+	)
 }
